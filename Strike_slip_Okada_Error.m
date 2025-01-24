7eee06ee62887039b438dc5a86e9098ce312cc92
Dibyashakti Panda <123026357+dibyashakti1@users.noreply.github.com>
2025-01-24 20:16:00 +0530

    Update Strike_slip_Okada_Error.m

--- Strike_slip_Okada_Error.m
+++ Strike_slip_Okada_Error.m
@@ -1,106 +1,150 @@
-% The matlab code help to model the observed velocities from GPS stations across srike-slip faults 
+% The matlab code help to model the observed velocities from GPS stations across strike-slip faults 
 % by analyzing misfit between the fault Slip rate & Locking Depth (Based on methods by Okada 1992)
 
-% Misfit analysis between the observed and modelled GPS velocities for strike-slip faults 
-% (Variable: Slip rate & Locking Depth ; Constant: Dip & Vertical Offset uY)
+% Misfit analysis between the observed and modelled GPS velocities for strike-slip faults
+% (Variable: Slip rate & Locking Depth; Constant: Dip & Vertical Offset uY)
 
-% Last modified on: 26 April, 2023 by Dibyashakti
+% Input: File must contain three columns (atleast) 
+% Distance from Fault Trace, Displacement, Uncertainity
+
+% Last modified on: 21 Jan, 2025 by D. Panda
 
 
 clear all
 close all
 clc
+addpath(genpath(pwd))
 
 %%
-% To visualize motion at surface
+% Load GNSS data
+[file,location] = uigetfile('*');
 
-fault=[0,-1e9; 0,1e9];
-dip=89*pi/180;
-depth=[0e3,15e3];
-B=20;
-type='S';
-Mu=30e9;
-Poisson=0.25;
+Fault_slip=readtable(fullfile(location,file));
 
-nobs=100;
-x=linspace(-500e3,500e3,nobs*2);
-y=0*x;
-[X,Y]=meshgrid(x,y);
-Z=0*X-0e15;
-
-[uX,uY,uZ]=Okada1992(X,Y,Z,fault,dip,depth,B,type,Mu,Poisson);
-
-% uY(X<0)=uY(X<0)-(B/2);
-% uY(X>0)=uY(X>0)+(B/2);
-figure(1),clf
-quiver(x/1e3,y/1e3,uX,uY,0,'color','k')
-xlim([-500,500])
-ylim([-20,20])
-hold off
-
+if width(Fault_slip)<3
+    error('File should contain at least 3 columns (Distance, Displacement, Error)')
+else
+    disp("Input File contains 3 columns > Distance, Displacement, Error")
+Fault_slip=sortrows(Fault_slip);
+dist=table2array(Fault_slip(:,1));
+slip=table2array(Fault_slip(:,2));
+error=table2array(Fault_slip(:,3));
+plot(dist,slip,'-bdiamond','linewidth',1,'DisplayName','Observed')
+hold on
+errorbar(dist,slip,error,'.b','DisplayName','Observed')
+title('Observed Displacement (mm/year)')
+% text(gps_nrsc.Long,gps_nrsc.Lat,gps_nrsc.Station,'Vert','bottom', 'Horiz','left','FontSize',7)
+end
 %%
 
-% Generate Okada dislocation (slip rate deficit curve)
-nobs=100;
-x=linspace(-100e3,100e3,nobs);
-y=0*x+0;
-z=0*x;
+menuop = menu('EXCLUSION OF ONE OR MORE POINTS FROM CALCULATIONS','YES','NO');
 
-[uX,uY,uZ]=Okada1992(x,y,z,fault,dip,depth,B,type,Mu,Poisson);
+if menuop ==1
 
-uY(x<0)=uY(x<0)-(B/2);
-uY(x>0)=uY(x>0)+(B/2);
-figure(2),clf
-plot(x/1e3,uY+(B/2),'r','linewidth',1)
+    plot(dist,slip,'-bdiamond','linewidth',1,'DisplayName','Observed')
+    hold on
+    errorbar(dist,slip,error,'.b','DisplayName','Observed')
 
-hold off
+    [rx,ry]=getpts;
+    dist_range=2;
+    slip_range=2;
+    id=find(dist<=fix(rx)+dist_range & dist>=fix(rx)-dist_range);
+    id2=find(slip<=fix(ry)+slip_range & slip>=fix(ry)-slip_range);
 
-%%
+    if length(id)<length(id2)
+        
+        plot(dist,slip,'-bdiamond','linewidth',1,'DisplayName','Observed')
+        hold on
+        plot(dist(id),slip(id),'-rx','linewidth',1,'DisplayName','Observed')
 
-% Misfit analysis between observed and modelled GPS velocities for strike-slip faults
-% (Variable: Slip rate & Locking Depth ; Constant: Dip & Vertical Offset uY)
+        dist_new=dist;
+        dist_new(id,:)=[];
 
-Fault_slip=load('SF_profile_Vigny_sites.txt');
-dist=Fault_slip(:,1);
-slip=Fault_slip(:,2);
-error=Fault_slip(:,3);
-errorbar(dist,slip,error)
+        slip_new=slip;
+        slip_new(id,:)=[];
 
-hold off
+        error_new=error;
+        error_new(id,:)=[];
 
-RMSE1=[];
+    else
 
-fprintf('Estimating RMSE by varying fault slip rate and locking depth...\n')
+        dist_new=dist;
+        dist_new(id2,:)=[];
 
-for i=0:1000:40000   % Locking depth (in meters)
-    for j=0:40       % Fault slip rate (in mm)
+        slip_new=slip;
+        slip_new(id2,:)=[];
 
-fault=[0,1e9; 0,-1e9];
-dip1=89;             % Dip of the fault
-dip=89*pi/180;
-depth=[0e3,i];
-B=j;
-type='S';
-Mu=30e9;
-Poisson=0.25;
-nobs=18;             % Number of GPS stations
-x=dist*1e3;
-y=0*x+0;
-z=0*x;
+        error_new=error;
+        error_new(id2,:)=[];
 
-[uX,uY,uZ]=Okada1992(x,y,z,fault,dip,depth,B,type,Mu,Poisson);
+    figure (1)
+    plot(dist,slip,'-bdiamond','linewidth',1,'DisplayName','Observed')
+    hold on
+    plot(dist(id2),slip(id2),'-rx','linewidth',1,'DisplayName','Observed')
 
-uY(x<0)=uY(x<0)-(B/2);
-uY(x>0)=uY(x>0)+(B/2);
-% plot(x/1e3,uY+(B/2),'r','linewidth',1)
-uY=uY+(B/2);
+    end
 
-     RMSE1 = [RMSE1;i/1000,j,(1/nobs)*sqrt(sum((slip(:)-uY(:)).^2./error.^2))];  % RMSE estimation between observed and modelled GPS velocities
+else
 
-    end 
-    uY=[]; 
+    dist_new=dist;
+    slip_new=slip;
+    error_new=error;
+
 end
 
+hold off
+figure (2)
+plot(dist,slip,'-bdiamond','linewidth',1,'DisplayName','Actual Stations')
+hold on
+plot(dist_new,slip_new,'-rdiamond','linewidth',1,'DisplayName','After Removal')
+legend
+
+%%  Estimate the RMSE misfit
+
+% Misfit between observed and modelled GNSS velocities for dip-slip faults
+% (Variable: Slip rate & Locking Depth ; Constant: Dip & Vertical Offset uY)
+
+plot(dist_new,slip_new,'-rdiamond','linewidth',1,'DisplayName','After Removal')
+hold on
+errorbar(dist_new,slip_new,error_new,'.r','DisplayName','Observed')
+title('Final Observed Displacement (mm/year)')
+
+% Modify hard coded parameters
+RMSE1 = [];
+type = 'S'; % Type of fault (Dip slip: 'D', Strike slip: 'S')
+dip1 = 10;  % Dip of the fault
+Mu = 30e9;  % Shear Modulus
+Poisson = 0.25;
+nobs = length(dist_new);    % Number of GPS stations
+LDival = 100;     %    Locking Depth interval, in meters
+slipival = 0.2;    %   Slip rate interval, in mm
+
+fprintf('Estimating RMSE by varying fault slip rate and locking depth...\n')
+tic
+for i = 0:LDival:40000       % Locking depth (in meters)
+    
+    for j = 0:slipival:25     % Fault slip rate (in mm)
+        
+        fault = [-1e9,0;1e9,0];
+        dip = (180-dip1)*pi/180;
+        depth = [0e3,i];
+        B = j;
+        y = dist_new*1e3;
+        x = 0*y;
+        z = 0*y;
+
+        [uX,uY,uZ] = Okada1992(x,y,z,fault,dip,depth,B,type,Mu,Poisson);
+        uY(y>0) = uY(y>0)+(B*cosd(dip1));
+
+        % RMSE estimation between observed and modelled GPS velocities
+        %    RMSE1 = [RMSE1;i/1000,(i/1000)/sind(dip),j,sqrt(mean((slip(:)-uY(:)).^2))];
+        RMSE1 = [RMSE1;i/1000,j,(1/nobs)*sqrt(sum((slip_new(:)-uY(:)).^2./error_new.^2))];
+    end
+
+    uY=[];
+
+end
+toc
 fprintf('Estimating RMSE by varying fault slip rate and locking depth...Done\n')
 
 %%
@@ -108,10 +152,12 @@ fprintf('Estimating RMSE by varying fault slip rate and locking depth...Done\n')
 x2=RMSE1(:,1);
 y2=RMSE1(:,2);
 z2=RMSE1(:,3);
-[X,Y]=meshgrid(min(x2):max(x2),min(y2):max(y2));
+xival=0.2;      % x interval for the mesh
+yival=0.2;      % y interval for the mesh
+[X,Y]=meshgrid(min(x2):xival:max(x2),min(y2):yival:max(y2));
 Z=griddata(x2,y2,z2,X,Y);
 
-figure(1),clf
+figure(3),clf
 contourf(X, Y, Z)
 minError = find(Z == min(Z(:)));
 minX=X(minError);
@@ -119,35 +165,34 @@ minY=Y(minError);
 hold on
 plot(minX,minY,'o','color','red')
 
-
-figure(2),clf
+figure(4),clf
 [uX,uY,uZ]=Okada1992(x,y,z,fault,dip,[0,minX*1e3],minY,type,Mu,Poisson);
-uY(x>0)=uY(x>0)+(minY);
-plot(x/1e3,uY,'-rx','linewidth',1,'DisplayName','Modelled')
+uY(y>0)=uY(y>0)+(minY*cosd(dip1));
+% uY=uY+1;
+plot(y/1e3,uY,'-bx','linewidth',1,'DisplayName','Modelled')
 hold on
-errorbar(dist,slip,error,'DisplayName','Observed')
-xlim([-200,200])
+errorbar(dist_new,slip_new,error_new,'DisplayName','Observed')
+xlim([-300,300])
 legend ('location','northwest')
 
-figure(3),clf
+figure(5),clf
 nobs=200;
-x=linspace(-500e3,500e3,nobs*2);
-y=0*x+0;
-z=0*x;
+y=linspace(-500e3,500e3,nobs*2);
+x=0*y;
+z=0*y;
 [uX,uY,uZ]=Okada1992(x,y,z,fault,dip,[0,minX*1e3],minY,type,Mu,Poisson);
-uY(x>0)=uY(x>0)+(minY);
+uY(y>0)=uY(y>0)+(minY*cosd(dip1));
 
-legend(plot(x/1e3,uY,'g','linewidth',1,'DisplayName','Best Fit'))
+legend(plot(y/1e3,uY,'g','linewidth',1,'DisplayName','Best Fit'))
 hold on
-errorbar(dist,slip,error,'DisplayName','Observed')
-xlim([-200,200])
+errorbar(dist_new,slip_new,error_new,'DisplayName','Observed')
+xlim([-300,300])
 legend ('location','northwest')
 
-% plot(nan, nan, 'DisplayName', "Dip = " + dip1)
+title(['Dip =  ' num2str(dip1), ' ; Locking Depth (km) = ' num2str(minX), ' ; Slip (mm) = ' num2str(minY)])
+% title(['Dip =  ' num2str(dip1), ' ; Locking Width (km) = ' num2str(minX/tand(dip1)), ' ; Slip (mm) = ' num2str(minY)])
 
-title(['Dip =  ' num2str(dip1), ' ; Locking Depth = ' num2str(minX), ' ; Slip = ' num2str(minY)])
-
-
+%%
 % Write RMSE and Okada Locking curve files
 fprintf('Saving file: Misfit between fault slip rate and locking depth...Done\n')
 % save('SS_RMSE.txt','RMSE1','-ascii');
